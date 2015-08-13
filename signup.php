0949a3f8235671b948d12370e907ca8772160f59
Mark Lippert <mark@foresitegrp.com>
2015-08-13 14:03:04 -0500

    Add MailChimp signup form

--- signup.php
+++ signup.php
@@ -4,9 +4,10 @@
     <span>Stay up to date on Belardi News &amp; Events.</span>
   </div>
 
-  <form class="vert-center">
-    <input type="text" placeholder="Email Address">
-    <input type="submit">
+  <form action="//belardiautoracing.us11.list-manage.com/subscribe/post?u=74a0de22787af56cf423f00a8&amp;id=8410e9df46" method="post" class="vert-center">
+    <input type="text" value="" name="EMAIL" placeholder="Email Address">
+    <input type="text" name="b_74a0de22787af56cf423f00a8_8410e9df46" tabindex="-1" value="" style="position: absolute; left: -5000px;">
+    <input type="submit" value="SUBMIT" name="subscribe">
   </form>
 
   <div style="clear: both;"></div>
