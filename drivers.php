8052531cb1f2c1cc0ff516f2c77ed73a2acb123f
foresitegroup <mark@foresitegrp.com>
2016-01-06 14:26:17 -0600

    Add new driver

--- drivers.php
+++ drivers.php
@@ -17,6 +17,56 @@ include "header.php";
 
 <div class="black drivers">
   <div class="content-width">
+    <div class="sideline"><h2>Zach Veach<!--  <span style="color: #FFFFFF;">#4</span> --></h2></div>
+
+    <div class="three-col">
+      <img src="images/driver-zach-veach.jpg" alt="Zach Veach">
+    </div>
+
+    <div class="three-col">
+      <strong>Nationality:</strong> American<br>
+      <strong>Birthdate:</strong> December 9, 1994<br>
+      <strong>Height/Weight:</strong> 5'5" / 125 lbs.<br>
+      <strong>Off the Track:</strong> Rock Climbing, Training, Activist For Anti-Bullying And Distracted Driving, Writer And Author.<br>
+      <br>
+
+      Veach, Ohio native, started racing karts at the age of twelve winning several Ohio championships. Three years later, he made the jump to open wheel cars at the age of 15 with Andretti Autosport. Competing in USF2000, he started his journey on the Mazda Road to Indy. Six years later, Veach has two years of USF2000, one year of Pro Mazda, and two years Of Indy Lights experience under his belt. After coming close to winning the 2014 Indy Lights Championship title, Veach is looking onto 2016 with high expectations!<br>
+      <br>
+
+      In 2016 Veach drives for Belardi Auto Racing in the Indy Lights Series.<br>
+      <br>
+
+      <a href="http://www.zachveach.com" class="linkarrow"><strong>WWW.ZACHVEACH.COM</strong></a>
+    </div>
+
+    <div class="three-col last">
+      <h3><a href="https://twitter.com/ZachVeach">@ZACHVEACH</a></h3>
+      
+      <a href="http://instagram.com/zachveach"><img src="images/driver-zach-veach-instagram.jpg" alt="" style="margin: 1.3em 0 0.75em;"></a>
+      <!-- <script type="text/javascript">
+        // var userFeed = new Instafeed({
+        //   get: 'user',
+        //   userId: 189928262,
+        //   accessToken: '36105938.467ede5.271adab5a0bc423aade7dfc6f6f791f1',
+        //   limit: 12,
+        //   template: '<a href="{{link}}" target="new"><img src="{{image}}" /></a>',
+        //   target: 'if-veach'
+        // });
+        // userFeed.run();
+      </script>
+      <div id="if-veach" class="instafeed"></div> -->
+
+      <div class="soclinks">
+        <a href="https://www.facebook.com/ZachVeachRacing">FACEBOOK</a> | <a href="http://www.twitter.com/ZachVeach">TWITTER</a> | <a href="http://instagram.com/zachveach">INSTAGRAM</a>
+      </div>
+    </div>
+
+    <div style="clear: both;"></div>
+
+    <br>
+    <br>
+    <br>
+<!--
     <div class="sideline"><h2>Felix Serrall&eacute;s <span style="color: #FFFFFF;">#4</span></h2></div>
 
     <div class="three-col">
@@ -114,7 +164,7 @@ include "header.php";
     <br>
     <br>
     <br>
-
+-->
     <hr>
 
     <br>
