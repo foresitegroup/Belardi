d1f17b35e2c31652936859bef9f9efbadc20edbb
foresitegroup <mark@foresitegrp.com>
2016-11-01 14:29:00 -0500

    Images are now drawn from blog featured images

--- media.php
+++ media.php
@@ -13,31 +13,33 @@ include "header.php";
     <br>
     <br>
 
-    <a href="images/gallery/media1.jpg" rel="m" class="fancybox two-col" style="background-image: url(images/gallery/media1.jpg);"></a>
-    <a href="images/gallery/media2.jpg" rel="m" class="fancybox two-col last" style="background-image: url(images/gallery/media2.jpg);"></a>
-    <div style="clear: both;"></div>
+    <?php
+    require('news/wp-blog-header.php');
+    $posts = get_posts('posts_per_page=14&order=DESC&orderby=date&meta_key=_thumbnail_id');
 
-    <a href="images/gallery/media3.jpg" rel="m" class="fancybox one-col" style="background-image: url(images/gallery/media3.jpg);"></a>
+    $i = 1;
 
-    <a href="images/gallery/media4.jpg" rel="m" class="fancybox three-col" style="background-image: url(images/gallery/media4.jpg);"></a>
-    <a href="images/gallery/media5.jpg" rel="m" class="fancybox three-col" style="background-image: url(images/gallery/media5.jpg);"></a>
-    <a href="images/gallery/media6.jpg" rel="m" class="fancybox three-col last" style="background-image: url(images/gallery/media6.jpg);"></a>
-    <div style="clear: both;"></div>
+    foreach ($posts as $post) :
+      setup_postdata($post);
 
-    <a href="images/gallery/media7.jpg" rel="m" class="fancybox two-col" style="background-image: url(images/gallery/media7.jpg);"></a>
-    <a href="images/gallery/media8.jpg" rel="m" class="fancybox two-col last" style="background-image: url(images/gallery/media8.jpg);"></a>
-    <div style="clear: both;"></div>
+      if ($i == 1 || $i == 7 || $i == 13)
+      echo "<a href=\"" . wp_get_attachment_url(get_post_thumbnail_id()) . "\" rel=\"m\" class=\"fancybox two-col\" style=\"background-image: url(" . wp_get_attachment_url(get_post_thumbnail_id()) . ");\"></a>\n";
 
-    <a href="images/gallery/media9.jpg" rel="m" class="fancybox one-col" style="background-image: url(images/gallery/media9.jpg);"></a>
+      if ($i == 2 || $i == 8 || $i == 14)
+      echo "<a href=\"" . wp_get_attachment_url(get_post_thumbnail_id()) . "\" rel=\"m\" class=\"fancybox two-col last\" style=\"background-image: url(" . wp_get_attachment_url(get_post_thumbnail_id()) . ");\"></a>\n<div style=\"clear: both;\"></div>\n";
 
-    <a href="images/gallery/media12.jpg" rel="m" class="fancybox three-col" style="background-image: url(images/gallery/media12.jpg);"></a>
-    <a href="images/gallery/media13.jpg" rel="m" class="fancybox three-col" style="background-image: url(images/gallery/media13.jpg);"></a>
-    <a href="images/gallery/media14.jpg" rel="m" class="fancybox three-col last" style="background-image: url(images/gallery/media14.jpg);"></a>
-    <div style="clear: both;"></div>
+      if ($i == 3 || $i == 9)
+      echo "<a href=\"" . wp_get_attachment_url(get_post_thumbnail_id()) . "\" rel=\"m\" class=\"fancybox one-col\" style=\"background-image: url(" . wp_get_attachment_url(get_post_thumbnail_id()) . ");\"></a>\n";
 
-    <a href="images/gallery/media10.jpg" rel="m" class="fancybox two-col" style="background-image: url(images/gallery/media10.jpg);"></a>
-    <a href="images/gallery/media11.jpg" rel="m" class="fancybox two-col last" style="background-image: url(images/gallery/media11.jpg);"></a>
-    <div style="clear: both;"></div>
+      if ($i == 4 || $i == 5 || $i == 10 || $i == 11)
+      echo "<a href=\"" . wp_get_attachment_url(get_post_thumbnail_id()) . "\" rel=\"m\" class=\"fancybox three-col\" style=\"background-image: url(" . wp_get_attachment_url(get_post_thumbnail_id()) . ");\"></a>\n";
+
+      if ($i == 6 || $i == 12)
+      echo "<a href=\"" . wp_get_attachment_url(get_post_thumbnail_id()) . "\" rel=\"m\" class=\"fancybox three-col last\" style=\"background-image: url(" . wp_get_attachment_url(get_post_thumbnail_id()) . ");\"></a>\n<div style=\"clear: both;\"></div>\n";
+
+      $i++;
+    endforeach;
+    ?>
 
     <br>
 
