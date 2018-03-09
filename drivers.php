97f1f1cf2b6e65024d2fa5e0a760ecb7c9d6f3f5
foresitegroup <mark@foresitegrp.com>
2018-03-09 13:06:13 -0600

    Add new driver

--- drivers.php
+++ drivers.php
@@ -15,6 +15,72 @@ include "header.php";
 
 <div class="black drivers">
   <div class="content-width">
+    <div class="sideline"><h2>Santiago Urrutia</h2></div>
+
+    <div class="three-col">
+      <img src="images/driver-santiago-urrutia.jpg" alt="Santiago Urrutia">
+    </div>
+
+    <div class="three-col">
+      <strong>Nationality:</strong> Uruguayan<br>
+      <strong>Birthdate:</strong> August 30, 1996<br>
+      <strong>Height/Weight:</strong> 5'7" / 149 lbs.<br>
+      <strong>Off the Track:</strong> rock/pop music, movies, motocross, handball, soccer<br>
+      <br>
+
+      Santiago was born in Miguelete, Uruguay.  He began his racing career in the Uruguayan National Championship Motorcycle Land Speed and started karting in 2002.  In his first year of karting, Santiago became the youngest to win the Uruguayan karting competition and ended in the National Championship Cadets category.<br>
+      <br>
+
+      2008 - Won the Argentine Karting Championship Junior Pre category as well as the Apetura Championship Metropolitan Prokart Mini Junior category.<br>
+      <br>
+
+      2009 - Earned the title of National Champion Master Category and added the title of Champion Federation in the same category.<br>
+      <br>
+
+      2010 - Traveled to Europe to compete in the World Karting Championship.<br>
+      <br>
+
+      2011 - Competed in the European Championship and Italian Formula Abarth for the BVM team.  Was crowned European Champion Rookie with 11 podiums and 3 wins.<br>
+      <br>
+
+      2013 - Joined RP Motorsport to contest the season of the European F3 Open.<br>
+      <br>
+
+      2014 - Signed with Koiranen GP GP3 Series.<br>
+      <br>
+
+      2015 - Champion in the Pro Mazda Championship presented by Cooper Tires series.<br>
+      <br>
+
+      2016 - Runner-up in the Indy Lights Championship presented by Cooper Tires series with 4 wins, 7 podiums, 13 top-fives and 14 top-ten finishes.<br>
+      <br>
+
+      <a href="http://santiurrutia.com.uy" class="linkarrow"><strong>WWW.SANTIURRUTIA.COM.UY</strong></a>
+    </div>
+
+    <div class="three-col last">
+      <h3><a href="https://twitter.com/santi_urrutia">@SANTI_URRUTIA</a></h3>
+
+      <div class="instagram-grid">
+        <?php
+        $json = json_decode(file_get_contents('https://www.instagram.com/santi_urrutia/?__a=1'));
+        foreach ($json->user->media->nodes as $key => $value) {
+          echo '<a href="'.'https://www.instagram.com/p/'.$value->code.'" style="background-image: url('.$value->display_src.');"></a>';
+        }
+        ?>
+      </div>
+
+      <div class="soclinks">
+        <a href="https://www.instagram.com/santi_urrutia">INSTAGRAM</a> | <a href="https://www.facebook.com/Santiago-Urrutia-Oficial-423891990978352/">FACEBOOK</a> | <a href="https://twitter.com/santi_urrutia">TWITTER</a>
+      </div>
+    </div>
+
+    <div style="clear: both;"></div>
+
+    <br>
+    <br>
+    <br>
+
     <div class="sideline"><h2>Aaron Telitz</h2></div>
 
     <div class="three-col">
@@ -43,11 +109,7 @@ include "header.php";
 
       <div class="instagram-grid">
         <?php
-        // $media = Instagram::getMediaByUserID(36105938, 12);
-        // foreach($media as $key=>$value) {
-        //   echo '<a href="'.'https://www.instagram.com/p/'.$media[$key]->code.'" style="background-image: url('.$media[$key]->display_src.');"></a>';
-        // }
-        $json = json_decode(file_get_contents('https://www.instagram.com/belardiracing/?__a=1'));
+        $json = json_decode(file_get_contents('https://www.instagram.com/aarontelitz/?__a=1'));
         foreach ($json->user->media->nodes as $key => $value) {
           echo '<a href="'.'https://www.instagram.com/p/'.$value->code.'" style="background-image: url('.$value->display_src.');"></a>';
         }
@@ -55,7 +117,7 @@ include "header.php";
       </div>
 
       <div class="soclinks">
-        <a href="https://www.facebook.com/Aaron-Telitz-254146281295285/">FACEBOOK</a> | <a href="https://twitter.com/AaronTelitz">TWITTER</a>
+        <a href="https://www.instagram.com/aarontelitz">INSTAGRAM</a> | <a href="https://www.facebook.com/Aaron-Telitz-254146281295285/">FACEBOOK</a> | <a href="https://twitter.com/AaronTelitz">TWITTER</a>
       </div>
     </div>
 
